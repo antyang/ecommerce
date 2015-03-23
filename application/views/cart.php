7191b46a5792ee78d5583488fdb75bfa9f669ca9
Antony Yang <mrant0ny@gmail.com>
2015-03-23 14:14:05 -0700

    added shopping cart

---
+++ application/views/cart.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Cart Page</title>
+	<link rel="stylesheet" type="text/css" href="/assets/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="/assets/css/skeleton.css">
+	<style>
+		body{
+			margin-left: 20px;
+		}
+
+	</style>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="eight columns"><h3>eCommerce</h3></div>
+		<div class="four columns"><h3>Shopping Cart()</h3></div>
+	</div>
+
+<div class="row">
+	<table class="u-full-width">
+		<tr>
+			<th>Item</th>
+			<th>Price</th>
+			<th>Quantity</th>
+			<th>Total</th>
+		</tr>
+		<tr>
+			<td>Black Belt for staff</td>
+			<td>$9.99</td>
+			<td>1   <a href="/">udpate</a> </td>
+			<td>$9.99</td>
+		</tr>
+		<tr>
+			<td>Black Belt for staff</td>
+			<td>$9.99</td>
+			<td>1   <a href="/">udpate</a> </td>
+			<td>$9.99</td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td>Total qty</td>
+			<td>total $$</td>
+		</tr>
+	</table>
+</div>
+
+<div class="row">
+	<div class="six columns">
+		<form action="">
+			<h5>Shipping Information</h5>
+				<input type="text" placeholder="First Name">
+				<input type="text" placeholder="Last Name">
+				<input type="text" placeholder="Address">
+				<input type="text" placeholder="Address 2">
+				<input type="text" placeholder="City">
+				<input type="text" placeholder="State">
+				<input type="text" placeholder="Zipcode">
+		</form>
+	</div>
+
+	<div class="six columns">
+		<div class="ten columns">
+		<form action="">
+			<h5>Billing Information <br></h5>
+			<input type="checkbox" name="mirror"> Same as shipping<br>
+				<input type="text" placeholder="First Name">
+				<input type="text" placeholder="Last Name">
+				<input type="text" placeholder="Address">
+				<input type="text" placeholder="Address 2">
+				<input type="text" placeholder="City">
+				<input type="text" placeholder="State">
+				<input type="text" placeholder="Zipcode">
+				<input type="text" placeholder="Card">
+				<input type="text" placeholder="Security Code">
+				<input type="date" placeholder="Expiration">
+		</form>
+		<button>Continue Shopping</button>
+		<button>Buy</button>
+		</div>
+	</div>
+</div>
+
+</div>
+</body>
+</html>
