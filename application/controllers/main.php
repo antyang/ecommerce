a06e73ee18e4b2e570e263514ed9270932d437f8
Brian Townsend <mr.briantownsend@gmail.com>
2015-03-23 17:40:58 -0700

    Routes/Views Update 1.1

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -5,16 +5,33 @@ class Main extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		//$this->load->model('???');
 		//$this->output->enable_profiler();
 	}
 
 	public function index()
 	{
-        $this->load->view('edit_product');
+        $this->load->view('admin_dashboard');
 	}
 	public function edit(/*$id*/)
 	{
 		$this->load->view('edit_product');
+	}
+	public function cart()
+	{
+		$this->load->view('cart');
+	}
+	public function dashboard()
+	{
+		$this->load->view('admin_dashboard');
+	}
+	public function admin_orders()
+	{
+		$this->load->view('admin_orders');
+	}
+		public function admin_products()
+	{
+		$this->load->view('admin_products');
 	}
 }
 
