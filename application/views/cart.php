35e1c0a08b5d51c7d8a54d7b5378c76616ef8689
Joshua Huang <jphuangjr@gmail.com>
2015-03-25 12:04:24 -0700

    added billing jquery

--- application/views/cart.php
+++ application/views/cart.php
@@ -12,11 +12,28 @@
 
 		$(document).ready(function(){
 
+
+            $('#toggle').click(function() {
+
+                    $('#first_name_bill').val($('#first_name_ship').val());
+                    $('#last_name_bill').val($('#last_name_ship').val());
+                    $('#address1_bill').val($('#address1_ship').val());
+                    $('#address2_bill').val($('#address2_ship').val());
+                    $('#city_bill').val($('#city_ship').val());
+                    $('#state_bill').val($('#state_ship').val());
+                    $('#zipcode_bill').val($('#zipcode_ship').val());
+
+            });
+
+
+
 			$("#credit").click(function(){
 				$("#credit_payment").toggle();
+
 			});
  			$('#toggle').click(function(){
     			$("#toggle").replaceWith("<i class='fa fa-toggle-on' id='toggle'> Same as shipping</i>");
+
   				});
  			$(document).on('click', '.update', function(){
     			$(this).replaceWith("<i class='fa fa-refresh fa-spin update'></i>");
@@ -104,13 +121,13 @@
 			<div class="twelve columns">
 			<form action="">
 				<h5 id="shipping_info">Shipping Information</h5>
-					<input type="text" placeholder="First Name">
-					<input type="text" placeholder="Last Name">
-					<input type="text" placeholder="Address">
-					<input type="text" placeholder="Address 2">
-					<input type="text" placeholder="City">
-					<input type="text" placeholder="State">
-					<input type="text" placeholder="Zipcode">
+					<input type="text" placeholder="First Name" name="first_name_ship" id="first_name_ship">
+					<input type="text" placeholder="Last Name" name="last_name_ship" id="last_name_ship">
+					<input type="text" placeholder="Address" name="address1_ship" id="address1_ship">
+					<input type="text" placeholder="Address 2" name="address2_ship" id="address2_ship">
+					<input type="text" placeholder="City" name="city_ship" id="city_ship">
+					<input type="text" placeholder="State" name="state_ship" id="state_ship">
+					<input type="text" placeholder="Zipcode" name="zipcode_ship" id="zipcode_ship">
 			</form>
 			</div>
 		</div>
@@ -121,13 +138,13 @@
 					<h5 id="billing_info">Billing Information</h5>
 					<!-- <input type="checkbox" name="mirror"> Same as shipping<br> -->
 					<i class="fa fa-toggle-off" id="toggle"> Same as shipping</i><br>
-						<input type="text" placeholder="First Name">
-						<input type="text" placeholder="Last Name">
-						<input type="text" placeholder="Address">
-						<input type="text" placeholder="Address 2">
-						<input type="text" placeholder="City">
-						<input type="text" placeholder="State">
-						<input type="text" placeholder="Zipcode"><br>
+						<input type="text" placeholder="First Name" name="first_name_bill" id="first_name_bill">
+						<input type="text" placeholder="Last Name" name="last_name_bill" id="last_name_bill">
+						<input type="text" placeholder="Address" name="address1_bill" id="address1_bill">
+						<input type="text" placeholder="Address 2" name="address2_bill" id="address2_bill">
+						<input type="text" placeholder="City" name="city_bill" id="city_bill">
+						<input type="text" placeholder="State" name="state_bill" id="state_bill">
+						<input type="text" placeholder="Zipcode" name="zipcode_bill" id="zipcode_bill"><br>
 					<h6 style="margin:auto;">Choose your payment</h6>
 					<h3 id="credit" ><i class="fa fa-credit-card"></i></h3>
 					<h4 style="font-size:36px;"><a href=""><i class="fa fa-cc-paypal"></i></a></h4>		
